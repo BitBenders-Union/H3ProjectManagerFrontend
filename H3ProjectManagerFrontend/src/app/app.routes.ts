6470bf385c219dadcaa2e017e405c1a9de0ca800
Denpas404 <102860689+Denpas404@users.noreply.github.com>
2024-04-18 21:32:12 +0200

    login component

--- H3ProjectManagerFrontend/src/app/app.routes.ts
+++ H3ProjectManagerFrontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { FrontpageComponent } from './components/frontpage/frontpage.component';
 import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-  { path: '', component: FrontpageComponent},
+  { path: '', component: LoginComponent},
   { path: 'login', component: LoginComponent}
 
 
