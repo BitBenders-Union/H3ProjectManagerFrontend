502cda204f1724ce4a5e2de23c1fce31b1db6a1b
Jonas Østergaard <Jonasoe@hotmail.com>
2024-04-19 13:35:09 +0200

    init

--- H3ProjectManagerFrontend/src/app/app.routes.ts
+++ H3ProjectManagerFrontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { FrontpageComponent } from './components/frontpage/frontpage.component';
 import { LoginComponent } from './components/login/login.component';
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent},
-  { path: 'login', component: LoginComponent}
-
+  { path: 'login', component: LoginComponent},
+  { path: 'user-dashboard', component: UserDashboardComponent}
 
 ];
