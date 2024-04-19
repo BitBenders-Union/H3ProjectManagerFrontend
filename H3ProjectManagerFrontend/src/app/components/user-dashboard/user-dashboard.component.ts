b9aaeef96f263990f2a8b21769bc6bb93213342e
Jonas Østergaard <Jonasoe@hotmail.com>
2024-04-19 14:14:07 +0200

    project component

--- H3ProjectManagerFrontend/src/app/components/user-dashboard/user-dashboard.component.ts
+++ H3ProjectManagerFrontend/src/app/components/user-dashboard/user-dashboard.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { ProjectComponent } from '../project/project.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    ProjectComponent,
+    CommonModule,
+
+  ],
   templateUrl: './user-dashboard.component.html',
   styleUrl: './user-dashboard.component.css'
 })
 export class UserDashboardComponent {
+
+
+  ownProjects: any[] = [];
+
+  othersProjects: any[] = [];
+
+  ngOnInit(){
+
+    this.ownProjects = this.getProjects();
+    this.othersProjects = this.getProjects();
+  }
+
+  // get a list of projects, change to api call
+  getProjects(){
+
+    return [0, 0, 0];
+  }
 
 }
