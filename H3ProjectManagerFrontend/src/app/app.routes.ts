1bfeb7616bf32c4884e5eabc72bba6b1cd8a60be
Dennis Paaske <102860689+Denpas404@users.noreply.github.com>
2024-04-22 08:57:03 +0200

    UserReg design done
    Co-Authored-By: Jonas Østergaard <98744422+jona339e@users.noreply.github.com>

--- H3ProjectManagerFrontend/src/app/app.routes.ts
+++ H3ProjectManagerFrontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { FrontpageComponent } from './components/frontpage/frontpage.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { UserRegistrationComponent } from './components/userRegistration/userRegistration.component';
 
 export const routes: Routes = [
   { path: '', component: FrontpageComponent},
   { path: 'login', component: LoginComponent},
+  {path: 'bruger-registrering', component: UserRegistrationComponent},
   { path: 'user-dashboard', component: UserDashboardComponent}
 
 ];
