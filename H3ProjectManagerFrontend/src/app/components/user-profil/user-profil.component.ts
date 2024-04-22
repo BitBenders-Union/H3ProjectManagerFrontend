fbe9822533095c8c97bc107c6baeeeb57d7a95c0
Dennis Paaske <102860689+Denpas404@users.noreply.github.com>
2024-04-22 12:24:36 +0200

    User profil
    Co-Authored-By: Jonas Østergaard <98744422+jona339e@users.noreply.github.com>

--- H3ProjectManagerFrontend/src/app/components/user-profil/user-profil.component.ts
+++ H3ProjectManagerFrontend/src/app/components/user-profil/user-profil.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-user-profil',
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './user-profil.component.html',
   styleUrls: ['./user-profil.component.css']
 })
 export class UserProfilComponent implements OnInit {
+
+  afdelinger : any[] = [];
 
   constructor() { }
 
