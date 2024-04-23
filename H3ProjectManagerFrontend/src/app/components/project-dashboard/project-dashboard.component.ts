a37774847654c7f9ec852ad7ce35303801ca3ece
Jonas Østergaard <jona339e@elev.tec.dk>
2024-04-23 09:57:10 +0200

    asdf

--- H3ProjectManagerFrontend/src/app/components/project-dashboard/project-dashboard.component.ts
+++ H3ProjectManagerFrontend/src/app/components/project-dashboard/project-dashboard.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './project-dashboard.component.html',
   styleUrl: './project-dashboard.component.css'
 })
 export class ProjectDashboardComponent {
   
+  projectList : any[] = [0, 0];
 
 }
