b147f7dbf3ad86fac62a4718376c80c95059d849
Jonas Østergaard <jona339e@elev.tec.dk>
2024-05-06 13:15:01 +0200

    upload

--- H3ProjectManagerFrontend/src/app/components/user-profil/user-profil.component.ts
+++ H3ProjectManagerFrontend/src/app/components/user-profil/user-profil.component.ts
@@ -1,25 +1,51 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { TokenService } from '../../service/token.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Department } from '../../models/Department';
 
 
 @Component({
   selector: 'app-user-profil',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   standalone: true,
   templateUrl: './user-profil.component.html',
   styleUrls: ['./user-profil.component.css']
 })
 export class UserProfilComponent implements OnInit {
 
-  afdelinger : any[] = [];
+  departments : Department[] = [];
+  user: User = this.token.getUserFromToken()!;
 
-  constructor() { }
+  detailsForm: FormGroup = new FormGroup({
+    id: new FormControl('', Validators.required),
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    username: new FormControl('', Validators.required),
+    creationDate: new FormControl('', Validators.required),
+    departmentsForm: new FormGroup({
+      id: new FormControl('', Validators.required),
+      name: new FormControl('', Validators.required),
+      }
+    )
+    }
+  );
+
+
+
+  constructor(private token: TokenService) { }
 
   ngOnInit() {
+
+    
   }
 
-  editProfil() : void {
-    console.log('editProfil');
+  editProfil() {
+
   }
 
 }
