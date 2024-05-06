d7f7b5970f8d4df46a2ed470cfc5c2f1e9a2e448
ChimpChonk <75368242+ChimpChonk@users.noreply.github.com>
2024-05-06 13:15:15 +0200

    edit task detail wip

---
+++ H3ProjectManagerFrontend/src/app/components/edit-project-details/edit-project-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-project-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-project-details.component.html',
+  styleUrl: './edit-project-details.component.css'
+})
+export class EditProjectDetailsComponent {
+
+}
