3d1359099bc81398d8f469666e560b5d1104560c
Jonas Østergaard <jona339e@elev.tec.dk>
2024-05-14 10:57:03 +0200

    task - create
    Co-Authored-By: Lucas Kramhöft <110811527+LucasLupin@users.noreply.github.com>
    Co-Authored-By: D.  Paaske <102860689+Denpas404@users.noreply.github.com>
    Co-Authored-By: ChimpChonk <75368242+ChimpChonk@users.noreply.github.com>

--- H3ProjectManagerFrontend/src/app/components/task-create/task-create.component.ts
+++ H3ProjectManagerFrontend/src/app/components/task-create/task-create.component.ts
@@ -1,22 +1,105 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ProjectTaskDetails } from '../../models/ProjectTask';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ProjectTaskStatus } from '../../models/ProjectTaskStatus';
+import { ProjectTaskCategory } from '../../models/ProjectTaskCategory';
+import { Priority } from '../../models/Priority';
+import { ApiGenericMethodsService } from '../../service/api-generic-methods.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-task-create',
   standalone: true,
   imports: [
-
+    CommonModule,
+    ReactiveFormsModule,
+    RouterLink
   ],
   templateUrl: './task-create.component.html',
   styleUrl: './task-create.component.css'
 })
 export class TaskCreateComponent {
+create() {
+throw new Error('Method not implemented.');
+}
 
-  
+
+  newTask?: ProjectTaskDetails;
 
   taskForm: FormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
+    description: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
+    projectId: new FormControl('', Validators.required),
+    priority: new FormControl('', Validators.required),
+    status: new FormControl('', Validators.required),
+    projectTaskCategory: new FormControl('', Validators.required),
+    users: new FormArray([]),
+    comments: new FormControl('', Validators.required),
 
   });
 
+
+  statusList: ProjectTaskStatus[] = [];
+  categoryList: ProjectTaskCategory[] = [];
+  priorityList: Priority[] = [];
+  userList: User[] = [];
+
+  constructor(private apiService: ApiGenericMethodsService) { }
+
+  ngOnInit(){
+    // get statuslist
+    // get categorylist
+    // get prioritylist
+
+    this.apiService.getAll<ProjectTaskStatus>('ProjectTaskStatus').subscribe({
+      next: (data) => {
+        this.statusList = data;
+      },
+      error: (error) => {
+        console.log(error.message);
+      }
+    });
+
+    this.apiService.getAll<ProjectTaskCategory>('ProjectTaskCategory').subscribe({
+      next: (data) => {
+        this.categoryList = data;
+      },
+      error: (error) => {
+        console.log(error.message);
+      }
+    });
+
+    this.apiService.getAll<Priority>('Priority').subscribe({
+      next: (data) => {
+        this.priorityList = data;
+      },
+      error: (error) => {
+        console.log(error.message);
+      }
+    });
+
+    this.apiService.getAll<User>('UserDetails').subscribe({
+      next: (data) => {
+        this.userList = data;
+        console.log(this.userList);
+        
+      },
+      error: (error) => {
+        console.log(error.message);
+      }
+    });
+
+
+  }
 
 }
