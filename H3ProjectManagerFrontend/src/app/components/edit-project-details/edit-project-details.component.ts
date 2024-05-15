9b50e4eb97d0427dab02d832a0c84541e038c889
ChimpChonk <sanjit.poudel28@gmail.com>
2024-05-15 21:31:39 +0200

    wip

--- H3ProjectManagerFrontend/src/app/components/edit-project-details/edit-project-details.component.ts
+++ H3ProjectManagerFrontend/src/app/components/edit-project-details/edit-project-details.component.ts
@@ -103,6 +103,12 @@ export class EditProjectDetailsComponent implements OnInit{
     });
   }
 
+  //Ng-Multi-Select-dropdown returns an array even if it's single select. This method converts the array
+  //to a single object
+  convertToSingleObject(slectedArray: any[]): any{
+    return slectedArray.length > 0 ? slectedArray[0] : null;
+  }
+
   getProjectDetails(id: number){
     this.service.getOne<ProjectDetails>('Project', id).subscribe({
       next: (data) => {
@@ -118,14 +124,25 @@ export class EditProjectDetailsComponent implements OnInit{
   }
 
   submit(){
+    const statusObject = this.convertToSingleObject(this.editForm.value.status);
+    const categoryObject = this.convertToSingleObject(this.editForm.value.category);
+    const priorityObject = this.convertToSingleObject(this.editForm.value.priority);
+
+    this.editForm.patchValue({
+      status: statusObject,
+      category: categoryObject,
+      priority: priorityObject
+    });
+
     this.sendProject = this.editForm.value;
+    this.sendProject!.owner = this.loc!.owner;
     console.log(this.sendProject);
-    this.api.update('Project', this.sendProject!).subscribe({
-      next: (data) => {
-        console.log(data);
-        this.route.navigate(['/proproject-details', this.id]);
-      }
-    })
+    // this.api.update('Project', this.sendProject!).subscribe({
+    //   next: (data) => {
+    //     console.log(data);
+    //     this.route.navigate(['/proproject-details', this.id]);
+    //   }
+    // })
   }
 
   getDepartments(){
