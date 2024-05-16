d2341522631ba3c82c7a8d08bbb40554e95cd28f
Dennis Paaske <102860689+Denpas404@users.noreply.github.com>
2024-05-16 10:28:50 +0200

    Fixed user in task vs user not in task
    Co-Authored-By: Jonas Østergaard <98744422+jona339e@users.noreply.github.com>
    Co-Authored-By: Lucas Kramhöft <110811527+LucasLupin@users.noreply.github.com>

--- H3ProjectManagerFrontend/src/app/components/task-details/task-details.component.ts
+++ H3ProjectManagerFrontend/src/app/components/task-details/task-details.component.ts
@@ -13,6 +13,7 @@ import { ApiGenericMethodsService } from '../../service/api-generic-methods.serv
 import { Priority } from '../../models/Priority';
 import { ProjectTaskCategory } from '../../models/ProjectTaskCategory';
 import { ProjectTaskStatus } from '../../models/ProjectTaskStatus';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-task-details',
@@ -23,19 +24,7 @@ import { ProjectTaskStatus } from '../../models/ProjectTaskStatus';
 })
 export class TaskDetailsComponent implements OnInit {
 
-  //Temp data start
-  users: any = [
-    { username: 'user1', firstname: 'John' },
-    { username: 'user2', firstname: 'Jane' },
-    { username: 'user3', firstname: 'Doe' },
-  ];
-
-  listOfUsers: any = [
-    { username: 'aliceDev', firstname: 'Alice' },
-    { username: 'bobQA', firstname: 'Bob' },
-    { username: 'charlieUX', firstname: 'Charlie' },
-  ];
-  //Temp data end
+  userNotInProject: User[] = []; // List of users NOT in the project
 
   priorityList: Priority[] = []; // List of priorities from the database
   statusList: ProjectTaskStatus[] = []; // List of statuses from the database
@@ -50,7 +39,7 @@ export class TaskDetailsComponent implements OnInit {
 
   constructor(
     private dataService: DataTransferServiceService,
-    private fb: FormBuilder, 
+    private fb: FormBuilder,
     private apiService: ApiGenericMethodsService
   ) {}
 
@@ -58,8 +47,6 @@ export class TaskDetailsComponent implements OnInit {
     // Get the task details from the project-details component using the data service
     this.dataService.currentMessage.subscribe((item) => {
       this.taskDetails = item as ProjectTaskDetails;
-      console.log(this.taskDetails); // Remember to remove this line
-      
     });
 
     // Initialize the form group
@@ -69,66 +56,92 @@ export class TaskDetailsComponent implements OnInit {
       taskPriority: ['', Validators.required],
       taskStatus: ['', Validators.required],
       taskCategory: ['', Validators.required],
-
     });
 
     // Get all the priorities, statuses and categories from the database
-    this.apiService.getAllSimple<Priority>('Priority').subscribe((data) => {
-      this.priorityList = data;          
+    this.apiService.getAllSimple<Priority>('Priority').subscribe({
+      next: (data) => {
+        this.priorityList = data;
+      },
+      error: (error) => {
+        console.log(error.message);
+      }
     });
 
-    this.apiService.getAllSimple<ProjectTaskStatus>('ProjectTaskStatus').subscribe((data) => {
-      this.statusList = data;      
+    this.apiService.getAllSimple<ProjectTaskStatus>('ProjectTaskStatus').subscribe({
+      next: (data) => {
+        this.statusList = data;
+      },
+      error: (error) => {
+        console.log(error.message);
+      }
     });
 
-    this.apiService.getAllSimple<ProjectTaskCategory>('ProjectTaskCategory').subscribe((data) => {
-      this.categoryList = data;      
+    this.apiService.getAllSimple<ProjectTaskCategory>('ProjectTaskCategory').subscribe({
+      next: (data) => {
+        this.categoryList = data;
+      },
+      error: (error) => {
+        console.log(error.message);
+      }
     });
 
-    
+    this.apiService.getAll<User>('UserDetails').subscribe({
+      next: (users) => {
+        this.userNotInProject = users.filter((user) => {
+          return !this.taskDetails.projectTaskUserDetail?.some((userDetail) => userDetail.id === user.id);
+        });
+      },
+      error: (error) => {
+        console.log(error.message);
+      }
+    });
+
   }
 
   // Toggle the visibility of the edit form
   toggleVisibility() {
     this.isEditing = !this.isEditing;
 
+
+
     // If the form is visible, populate the fields with the task details - makes it easier to edit / save / validate the form
     this.editForm.patchValue({
       taskName: this.taskDetails.name,
       taskDescription: this.taskDetails.description,
       taskPriority: this.priorityList.find(item => item.name === this.taskDetails.priority.name),
-      taskStatus: this.statusList.find(item => item.name === this.taskDetails.status.name), 
-      taskCategory: this.categoryList.find(item => item.name === this.taskDetails.projectTaskCategory.name),      
+      taskStatus: this.statusList.find(item => item.name === this.taskDetails.status.name),
+      taskCategory: this.categoryList.find(item => item.name === this.taskDetails.projectTaskCategory.name),
     });
   };
 
 
 
-  saveButton() {    
+  saveButton() {
     if (this.editForm.valid) { // Check if the form is valid
-      this.taskDetails.name = this.editForm.value.taskName;  
-      this.taskDetails.description = this.editForm.value.taskDescription; 
+      this.taskDetails.name = this.editForm.value.taskName;
+      this.taskDetails.description = this.editForm.value.taskDescription;
       this.taskDetails.priority.name = this.editForm.value.taskPriority.name;
-      this.taskDetails.status.name = this.editForm.value.taskStatus.name; 
+      this.taskDetails.status.name = this.editForm.value.taskStatus.name;
       this.taskDetails.projectTaskCategory.name = this.editForm.value.taskCategory.name;
-      
+
       // Reset the form and hide it
-      this.isEditing = false;      
+      this.isEditing = false;
+
+
     }
   }
 
 
   //  we need to redo this function to work with junction tables
   addUser(user: any) {
-    console.log('User added');
-    this.users.push(user)
-    this.listOfUsers.splice(this.listOfUsers.indexOf(user), 1);
+    this.taskDetails.projectTaskUserDetail?.push(user);
+    this.userNotInProject.splice(this.userNotInProject.indexOf(user), 1);
   }
 
   // we need to redo this function to work with junction tables
   removeUser(user : any) {
-    console.log('User removed');
-    this.users.splice(this.users.indexOf(user), 1);
-    this.listOfUsers.push(user);
+    this.taskDetails.projectTaskUserDetail?.splice( this.taskDetails.projectTaskUserDetail?.indexOf(user), 1);
+    this.userNotInProject.push(user);
   }
 }
